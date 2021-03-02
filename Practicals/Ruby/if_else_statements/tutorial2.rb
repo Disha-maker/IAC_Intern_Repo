6bcfd415fe4030ca67198fd079753c30264ce41c
Disha-maker <disha.kareer@puppet.com>
2021-03-02 11:38:53 +0000

    Adding tutorial 2 file with if else examples

---
+++ Practicals/Ruby/if_else_statements/tutorial2.rb
@@ -0,0 +1,16 @@
+  # if statement example
+x = 4
+if x > 3
+  puts "x is greater than 3"
+  puts "running this code block (puts)"
+end
+
+  # else statement example
+x = 2
+if x > 3
+  puts "x is greater than 3"
+  puts "executing lines of code (puts)"
+else
+  puts "other code block here now"
+  puts "condition (x > 3) is not true"
+end
